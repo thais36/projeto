5f00bc740bd279d471cac4ab2eb2a68097fcbf5f
thais36 <thais.almeida14@fatec.sp.gov.br>
2023-06-12 18:57:31 -0300

    alteirei a porta de 5000 para 3000 no server.js

--- reactapp/src/server.js
+++ reactapp/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Servir os arquivos estáticos do React
 app.use(express.static(path.join(__dirname, 'build')));
