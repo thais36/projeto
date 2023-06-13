19e10ba0ed782ee902939cdf9d9304196063eaf7
thais36 <thais.almeida14@fatec.sp.gov.br>
2023-06-12 22:38:39 -0300

    root

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -9,12 +9,19 @@ import {createRoot} from 'react-dom';
 
 // Exemplo de uso da função createRoot
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render( 
+  <React.StrictMode> 
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
